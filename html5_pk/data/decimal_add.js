977f5144f8e41836882ad9335b444d48a8ee592e
unknown <admin@.(none)>
2017-09-30 00:04:58 +0800

    add some old games

---
+++ html5_pk/data/decimal_add.js
@@ -0,0 +1,53 @@
+﻿//
+//開始比賽前, 說明對話框的說明文字
+//
+helpText = "請找出算式的答案。";
+
+
+
+decimal_places = 2;	//小數位數
+
+//基本設定
+optionsTotal = 6;		//共有幾個選項
+optionWidth = 100;		//每個選項的寬度
+optionHeight = 100;		//每個選項的高度
+optionColTotal = 3;		//一列有幾個選項
+
+/******************************************************************
+ 以下為程式碼, 不要更動
+ ******************************************************************/
+getOneQuestion = function(tools) {
+	//抽出一個數
+	var factor = Math.pow(10, decimal_places);
+	var tempRandom = tools.makeRandomIndex(1, factor-1);
+	var r = Math.floor(Math.random()*tempRandom.length);
+	var n1 = tempRandom[r];
+	//抽出一個數
+	var tempRandom = tools.makeRandomIndex(1, factor-1);
+	var r = Math.floor(Math.random()*tempRandom.length);
+	var n2 = tempRandom[r];	
+	//組合成題幹
+	var question = (n1/factor) + '+' + (n2/factor) + ' = ?';		//題幹
+	//計算答案
+	var answer = (n1 + n2)/factor;	//答案
+		
+	//製作其它非正解的選項
+	var min = -3;
+	//以亂數產生要由答案再加減多少
+	var nRandom = tools.makeRandomIndex(min, 9);	//其它選項比解答加減多少(用亂數排)
+
+	var op = new Object();
+	op.optionsOK = new Array();
+	op.optionsNG = new Array();
+	
+	op.question = question;
+	op.optionsOK[0] = answer;
+	var j = 0;
+	for(var i=0; i<nRandom.length; i++) {
+		var r = (n1 + n2 + nRandom[i])/factor;
+		if( r != 0 && r != answer) {
+			op.optionsNG[j++] = r;
+		}
+	}
+	return op;
+}
---
+++ html5_pk/pk_set.js
@@ -0,0 +1,86 @@
+﻿//
+
+menuSettingFilename = 'pk_menu_set.js';	//選單的檔名
+
+soundDataFilename = 'sound_data.js';	//聲音檔的檔名
+
+//設定
+questionsToAnswer = 10;	//一回合要回答幾題
+
+helpTextDefault = '兩邊都按完GO後開始PK';	//預設的說明文字
+
+//要 pk 多少的乘法表
+timesTables = new Array( 1, 2, 3, 4, 5, 6, 7, 8, 9 ); 
+
+scoreToAdd = 10;		//答完加減幾分
+
+playerHeight = 100;		//角色圖高
+rotation = 25;			//角色圖開始爬山要先旋轉幾度以符合爬山的姿態
+
+key_map = new Array();
+
+//Player1 的按鍵對照
+key_map[0] = new Array(
+	'Q', 'W', 'E', 'A', 'S', 'D'
+);	
+
+//Player2 的按鍵對照
+key_map[1] = new Array(
+	'NUM_SEVEN', 'NUM_EIGHT', 'NUM_NINE', 'NUM_FOUR', 'NUM_FIVE', 'NUM_SIX'
+);	
+
+
+
+/******************************************************************
+//鍵盤按鍵名稱與 keycode 的對照表
+//底下資料非必要,不要修改
+ ******************************************************************/
+keyCodes = {
+  0: 48,			//一般數字鍵(非九宮格)
+  1: 49,
+  2: 50,
+  3: 51,
+  4: 52,
+  5: 53,
+  6: 54,
+  7: 55,
+  8: 56,
+  9: 57,
+  A: 65,			//字母鍵
+  B: 66,
+  C: 67,
+  D: 68,
+  E: 69,
+  F: 70,
+  G: 71,
+  H: 72,
+  I: 73,
+  J: 74,
+  K: 75,
+  L: 76,
+  M: 77,
+  N: 78,
+  O: 79,
+  P: 80,
+  Q: 81,
+  R: 82,
+  S: 83,
+  T: 84,
+  U: 85,
+  V: 86,
+  W: 87,
+  X: 88,
+  Y: 89,
+  Z: 90,
+  NUM_ZERO: 96,		//鍵盤中九宮格的數字鍵代碼 NUM_xxx
+  NUM_ONE: 97,
+  NUM_TWO: 98,
+  NUM_THREE: 99,
+  NUM_FOUR: 100,
+  NUM_FIVE: 101,
+  NUM_SIX: 102,
+  NUM_SEVEN: 103,
+  NUM_EIGHT: 104,
+  NUM_NINE: 105
+};
+

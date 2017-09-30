93c8981b5b3a921738aa25bdca96c0717ebae923
GS Yan <gsyan888+github@gmail.com>
2017-10-01 02:39:06 +0800

    add listening

---
+++ html5_listening/sample_images_list.js
@@ -0,0 +1,9 @@
+﻿var URIformatVars = function(){/*--這一行請勿更改--
+cat.png
+chicken.png
+cow.png
+dog.png
+egg.png
+horse.png
+monkey.png
+-----*/}.toString().slice("function(){/*--這一行請勿更改--".length+2,-9);

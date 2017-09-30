ff917d51b3e4717e6ed5badf1ffbe7fc1ce46fcf
unknown <admin@.(none)>
2017-10-01 02:07:03 +0800

    upload A to L

---
+++ html5_basketball/basketball_set.js
@@ -0,0 +1,4 @@
+scoreToAdd = 10;		//答對時加幾分
+scoreToMinus = -10;	//答錯時扣幾分
+menuSettingFilename = 'basketball_menu_set.js';	//選單設定檔檔名
+
---
+++ html5_ghost/ghost_set.js
@@ -0,0 +1,21 @@
+/**
+ * 遊戲說明的設定
+ */ 
+
+ //標題 
+intro_title = '遊戲說明';
+
+//說明文字
+intro_message = '在時間內找出所有答案即可過關。加油！';
+//按鈕上的文字
+intro_button_text = '開始挑戰';	
+
+menuSettingFilename = 'ghost_menu_set.js';	//選單設定檔檔名
+
+
+defaultTimerStartSeconds = 30;	//計時器由幾秒開始倒數
+defaultHoldTime = 2;			//每個選項暫停移動等候按下的秒數
+
+scoreToAdd = 10;		//答對時加幾分
+scoreToMinus = -10;		//答錯時扣幾分
+

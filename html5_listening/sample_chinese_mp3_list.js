2120e39e457f7c64dc6b848fe69a6c282c948e6a
unknown <admin@.(none)>
2017-10-01 02:11:59 +0800

    rm listening

--- html5_listening/sample_chinese_mp3_list.js
+++
@@ -1,9 +0,0 @@
-﻿var URIformatVars = function(){/*--這一行請勿更改--
-小狗.mp3
-山羊.mp3
-帽子.mp3
-斑馬.mp3
-杯子.mp3
-被子.mp3
-貓咪.mp3
------*/}.toString().slice("function(){/*--這一行請勿更改--".length+2,-9);
--- html5_listening/sample_images_list.js
+++
@@ -1,9 +0,0 @@
-﻿var URIformatVars = function(){/*--這一行請勿更改--
-cat.png
-chicken.png
-cow.png
-dog.png
-egg.png
-horse.png
-monkey.png
------*/}.toString().slice("function(){/*--這一行請勿更改--".length+2,-9);

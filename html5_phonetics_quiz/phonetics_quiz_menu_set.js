885354e22f33d965c8fd73101993ffa99f29f4a9
GS Yan <gsyan888+github@gmail.com>
2017-10-01 02:41:47 +0800

    add html5_phonetics_quiz

---
+++ html5_phonetics_quiz/phonetics_quiz_menu_set.js
@@ -0,0 +1,5 @@
+menu_title='注音高手';
+menu_items = new Array(
+ '例1:不選國字,sample-set-1.js'
+,'例2:要選國字,sample-set-2.js'
+);
---
+++ html5_phonetics_quiz/sample-file-list.js
@@ -0,0 +1,15 @@
+﻿var URIformatVars = function(){/*--這一行請勿更改--
+小狗.mp3
+小狗.png
+山羊.mp3
+帽子.mp3
+帽子.png
+斑馬.mp3
+斑馬.png
+杯子.mp3
+杯子.png
+被子.mp3
+被子.png
+貓咪.mp3
+貓咪.png
+-----*/}.toString().slice("function(){/*--這一行請勿更改--".length+2,-9);

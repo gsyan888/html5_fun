c4371f3278b38cdefbd67cf3f112ed7e1b127acc
unknown <admin@.(none)>
2017-10-01 00:41:12 +0800

    update index.html

--- make_index.py
+++ make_index.py
@@ -33,7 +33,7 @@ def make_index_page() :
 
     header = """<html>
 <head>
-<title>HTML5 FUN : gsyan : ¶¯</title>
+<title>HTML5 FUN : gsyan : é›„</title>
 <meta content='text/html; charset=UTF-8' http-equiv='Content-Type'/>
 <style>
 h1 {

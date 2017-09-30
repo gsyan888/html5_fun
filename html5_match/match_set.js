bef7e3b2ca1f912b7f94418a4176fe43f832b79f
unknown <admin@.(none)>
2017-10-01 02:14:30 +0800

    upload match monster and phonetics_quiz

---
+++ html5_match/match_set.js
@@ -0,0 +1,25 @@
+title = 'TPET 對對碰';
+font = '標楷體';
+maxCol = 4;
+scoreAdd = 100;
+scoreMinus = 150;
+memoryMode = 'no';
+momorySeconds = 10;
+cards= new Array(
+  '不務正業是我的正業,http://avatars.plurk.com/3340703-big14.jpg'
+, 'Jen-Hsiung,http://avatars.plurk.com/4931391-big12.jpg'
+, '橡皮糖,http://avatars.plurk.com/3862532-big8.jpg'
+, '以柔克剛,http://avatars.plurk.com/4408804-big24.jpg'
+, '蠅狼glglace,http://avatars.plurk.com/3864274-big24.jpg'
+, 'kyt12,http://avatars.plurk.com/5447113-big12.jpg'
+, 'cherry9041,http://avatars.plurk.com/4996104-big8.jpg'
+, 'deediao,http://avatars.plurk.com/3728168-big20.jpg'
+, 'gsyan,http://avatars.plurk.com/4378698-big14.jpg'
+, 'shian賢言賢語,http://avatars.plurk.com/3468062-big4.jpg'
+, '謝KK,http://avatars.plurk.com/3638962-big16.jpg'
+, '阿欣,http://avatars.plurk.com/574362-big26.jpg'
+, 'ㄚ亮笑長,http://avatars.plurk.com/3906685-big12.jpg'
+, 'iCYY(哇係歪歪),http://avatars.plurk.com/3422079-big20.jpg'
+, '新竹瘋浪客,http://avatars.plurk.com/3566268-big10.jpg'
+, 'phcno1,http://avatars.plurk.com/4349352-big6.jpg'
+);

b16f11765d4727fd203a780f017bf523cf8dd2cf
GS Yan <gsyan888+github@gmail.com>
2017-10-01 22:22:13 +0800

    spelling add keylistenner and timer

--- html5_spelling/spelling_set.js
+++ html5_spelling/spelling_set.js
@@ -4,8 +4,8 @@
 &logger_url=&
 &datafolder=data&
 &menu=
-單字高手:表情,03.js
-單字高手:動物,01.js
+單字高手:動物(限時60秒),01.js
 單字高手:樂器,02.js
+單字高手:表情,03.js
 &
 -----*/}.toString().slice("function(){/*--這一行請勿更改--".length+2,-9);

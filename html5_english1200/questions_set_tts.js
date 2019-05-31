1e87e669cb20b84c21976d783227b8166741d9f2
GS Yan <gsyan888+github@gmail.com>
2019-06-01 06:29:31 +0800

    add english 1200

---
+++ html5_english1200/questions_set_tts.js
@@ -0,0 +1,58 @@
+﻿//每題有幾個選項
+numberOfOptions = 4;
+
+//每列最多可以有幾個選項
+numberOfOptionsPerRow = 2;	
+
+//每回合抽幾題題目
+numberOfQuestionsPerRound = 10;
+
+
+//聲音檔路徑的前置網址
+//也就是會為 questionLines 的左欄前面自動加上的字串
+//
+// English 1200
+// http://mail.ymps.tyc.edu.tw/english1200/
+// https://stuweb.syps.tp.edu.tw/english1200/
+//
+soundBaseURL = 'https://stuweb.syps.tp.edu.tw/english1200/';
+
+//是否使用 TTS 的語音
+tts_enabled = true;
+
+//----------------
+//Google TTS 文字轉語音的設定
+//----------------
+tts_language = 'en';  // en : 英語,   zh_tw : 中文
+tts_speed = 0.3;  //語音的速度 0 ~ 1 (可用小數)
+tts_base_url = 'https://translate.google.com/translate_tts?ie=UTF-8&tl='+tts_language+'&client=tw-ob&ttsspeed='+tts_speed+'&q=';
+
+//-------------------------------
+//用來偵測換行字元用的測試區塊
+//請勿更動
+//並且放在題庫設定區塊之前
+//-------------------------------
+//測試區塊開始
+CR_LF_test = function(){/*--這一行請勿更改--
+CR_LF testing block
+-----*/}.toString().slice("function(){/*--這一行請勿更改--".length+2,-9);
+CR_LF_First_Pos = CR_LF_test.indexOf('_LF')-'CR_LF'.indexOf('_LF');
+//測試區塊結束
+//-------------------------------
+
+
+//題庫
+//欄位分隔符號為兩個井字號(##)
+seperator = '##';
+
+//左欄為聲音檔路徑
+//右欄為中文
+questionLines = function(){/*--這一行請勿更改--
+cat##貓咪
+chicken##雞
+cow##牛
+dog##狗
+egg##蛋
+horse##馬
+monkey##猴子
+-----*/}.toString().slice("function(){/*--這一行請勿更改--".length+2+(typeof(CR_LF_First_Pos)!='undefined' ? CR_LF_First_Pos:0),-9);

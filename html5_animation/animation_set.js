19eb2280f2080cde03f5a3edb965cf6250d78c18
GS Yan <gsyan888+github@gmail.com>
2020-11-03 11:02:15 +0800

    add config file and show menu on the top of scence

---
+++ html5_animation/animation_set.js
@@ -0,0 +1,16 @@
+//遮罩圖檔的路徑
+maskFilename = "assets/animation-mask.png";
+
+//動畫圖檔的路徑清單
+menu_items = [ 
+			"assets/animation-frames.png"
+			, "assets/animation-frames-1.png"
+			, "assets/animation-frames-2.png"
+			, "assets/animation-frames-3.png"
+			, "assets/animation-frames-4.png"
+			, "assets/animation-frames-5.png"
+			, "assets/animation-frames-6.png"
+];
+
+//圖片來源的網址
+sourceURL = "https://openclipart.org/";

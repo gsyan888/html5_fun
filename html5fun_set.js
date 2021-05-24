e269c3eab9401183e2a039ce605669ba5bb29bd5
gsyan888 <gsyan888+github@gmail.com>
2021-05-24 14:30:56 +0800

    index.html changed to use HTML5

---
+++ html5fun_set.js
@@ -0,0 +1,234 @@
+caption = "HTML5 FUN BY GSYan";
+
+//選單
+//
+//caption , description, icon, demoURL, archiiveURL, blogURL
+//
+menu_items = [ 
+	[ "筆順練習第二版"
+	, "國字、注音筆順練習小工具"
+	, "html5_stroke/assets/icon.png"
+	, "html5_stroke/html5_stroke.html"
+	, ""
+	, "https://gsyan888.blogspot.com/2013/12/html5-stroke.html"
+	],
+
+	[ "筆順練習第三版"
+	, "Stroke Parts"
+	, "html5_stroke_parts/assets/icon.png"
+	, "html5_stroke_parts/html5_stroke_parts.html"
+	, ""
+	, ""
+	],
+
+	[ "筆順編輯器"
+	, "筆順造字小工具"
+	, "html5_strokeeditor/assets/icon.png"
+	, "html5_strokeeditor/html5_strokeeditor.html"
+	, ""
+	, "https://gsyan888.blogspot.com/2019/04/html5-stroke-editor.html"
+	],
+
+	[ "PK大賽"
+	, "兩人 PK 遊戲"
+	, "html5_pk/assets/icon.png"
+	, "html5_pk/html5_pk.html"
+	, "archives/html5_pk.zip"
+	, "https://gsyan888.blogspot.com/2012/11/html5-pk.html"
+	],
+
+	[ "PK2:四人PK"
+	, "最多可以四人同時 PK 的遊戲"
+	, "html5_pk2/assets/icon.png"
+	, "html5_pk2/html5_pk2.html"
+	, "archives/html5_pk2.zip"
+	, "https://gsyan888.blogspot.com/2013/12/html5-pk2.html"
+	],
+
+	[ "English1200"
+	, ""
+	, "html5_english1200/assets/icon.png"
+	, "html5_english1200/html5_english1200.html"
+	, "archives/html5_english1200.zip"
+	, "https://gsyan888.blogspot.com/2019/06/html5-english-1200.html"
+	],
+	
+	[ "王牌投手"
+	, ""
+	, "html5_baseball/assets/icon.png"
+	, "html5_baseball/html5_baseball.html"
+	, "archives/html5_baseball.zip"
+	, ""
+	],
+
+	[ "投籃高手"
+	, ""
+	, "html5_basketball/assets/icon.png"
+	, "html5_basketball/html5_basketball.html"
+	, "archives/html5_basketball.zip"
+	, ""
+	],
+
+	[ "打鬼遊戲"
+	, ""
+	, "html5_ghost/assets/icon.png"
+	, "html5_ghost/html5_ghost.html"
+	, "archives/html5_ghost.zip"
+	, "https://gsyan888.blogspot.com/2012/12/html5-ghost.html"
+	],
+
+	[ "搶救公主"
+	, "搶救公主大作戰"
+	, "html5_shark/assets/icon.png"
+	, "html5_shark/html5_shark.html"
+	, "archives/html5_shark.zip"
+	, ""
+	],
+
+	[ "量詞大挑戰"
+	, ""
+	, "html5_bubble/assets/icon.png"
+	, "html5_bubble/html5_bubble.html"
+	, "archives/html5_bubble.zip"
+	, ""
+	],
+
+	[ "Monster 語詞大挑戰"
+	, ""
+	, "html5_monster/assets/icon.png"
+	, "html5_monster/html5_monster.html"
+	, "archives/html5_monster.zip"
+	, ""
+	],
+	
+	[ "Bingo"
+	, ""
+	, "html5_bingo/assets/icon.png"
+	, "html5_bingo/html5_bingo.html"
+	, "archives/html5_bingo.zip"
+	, "https://gsyan888.blogspot.com/2017/10/html5-bingo.html"
+	],
+
+	[ "大家一起來"
+	, ""
+	, "html5_card_flip/assets/icon.png"
+	, "html5_card_flip/html5_card_flip.html"
+	, "archives/html5_card_flip.zip"
+	, "https://gsyan888.blogspot.com/2021/03/html5-card-flip.html"
+	],
+
+	[ "樂透機"
+	, ""
+	, "html5_lotto/assets/icon.png"
+	, "html5_lotto/html5_lotto.html"
+	,"archives/html5_lotto.zip"
+	,"https://gsyan888.blogspot.com/2012/11/html5-lotto.html"
+	],
+
+	[ "英文單字高手"
+	, "英文單字拼寫練習"
+	, "html5_spelling/assets/icon.png"
+	, "html5_spelling/html5_spelling.html"
+	, "archives/html5_spelling.zip"
+	, "https://gsyan888.blogspot.com/2014/08/html5-spelling.html"
+	],
+
+	[ "聽音辨字六選一"
+	, ""
+	, "html5_listening/assets/icon.png"
+	, "html5_listening/html5_listening.html"
+	, "archives/html5_listening.zip"
+	, ""
+	],
+
+	[ "漢字五子棋"
+	, ""
+	, "html5_gomoku/assets/icon.png"
+	, "html5_gomoku/html5_gomoku.html"
+	, "archives/html5_gomoku.zip"
+	, "https://gsyan888.blogspot.com/2021/03/html5-gomoku.html"
+	],
+	
+	[ "TPET造句靈感產生器"
+	, "翻卡即時創作"
+	, "html5_sentence/assets/icon.png"
+	, "html5_sentence/html5_sentence.html"
+	, "archives/html5_sentence.zip"
+	, "https://gsyan888.blogspot.com/2014/07/html5-sentence.html"
+	],
+	
+	[ "對對碰"
+	, ""
+	, "html5_match/assets/icon.png"
+	, "html5_match/html5_match.html"
+	, "archives/html5_match.zip"
+	, ""
+	],
+
+	[ "抽號碼機"
+	, ""
+	, "html5_number_selector/assets/icon.png"
+	, "html5_number_selector/html5_number_selector.html"
+	, "archives/html5_number_selector.zip"
+	, "https://gsyan888.blogspot.com/2012/11/html5-numberselector.html"
+	],
+
+	[ "幸運輪"
+	, "轉一轉、抽一抽"
+	, "html5_wheel/assets/icon.png"
+	, "html5_wheel/html5_wheel.html"
+	, "archives/html5_wheel.zip"
+	, "https://gsyan888.blogspot.com/2017/09/html5-wheel.html"
+	],
+	
+	[ "Draw Names 抽籤"
+	, ""
+	, "html5_drawnames/assets/icon.png"
+	, "html5_drawnames/html5_drawnames.html"
+	, "archives/html5_drawnames.zip"
+	, ""
+	],
+		
+	[ "戳戳樂"
+	, "戳戳樂、洞洞樂、抽抽看"
+	, "html5_poke/assets/icon.png"
+	, "html5_poke/html5_poke.html"
+	, "archives/html5_poke.zip"
+	, ""
+	],
+
+	[ "重組遊戲"
+	, "UNSCRAMBLE"
+	, "html5_unscramble/assets/icon.png"
+	, "html5_unscramble/html5_unscramble.html"
+	, "archives/html5_unscramble.zip"
+	, "https://gsyan888.blogspot.com/2019/04/html5-unscramble.html"
+	],
+	
+	[ "注音高手"
+	, "誰是注音拼音高手"
+	, "html5_phonetics_quiz/assets/icon.png"
+	, "html5_phonetics_quiz/html5_phonetics_quiz.html"
+	, "archives/html5_phonetics_quiz.zip"
+	, "https://gsyan888.blogspot.com/2014/02/html5-phoneticsquiz.html"
+	],
+
+	[ "Animation"
+	, "線上玩柵欄動畫"
+	, "html5_animation/assets/icon.png"
+	, "html5_animation/html5_animation.html"
+	, "archives/html5_animation.zip"
+	, ""
+	],
+
+	[ "AR Numbers"
+	,"使用鏡頭與卡片玩遊戲"
+	, "html5_ar_numbers/assets/icon.png"
+	, "html5_ar_numbers/html5_ar_numbers.html"
+	, "archives/html5_ar_numbers.zip"
+	, ""
+	]
+
+];
+
+

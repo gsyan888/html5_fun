f817a611d25314c81c0172bac27c606c486cdae0
gsyan888 <gsyan888+github@gmail.com>
2022-10-10 21:15:27 +0800

    _set.js bugfix

--- html5_phonetics_quiz/phonetics_quiz_set.js
+++ html5_phonetics_quiz/phonetics_quiz_set.js
@@ -15,34 +15,28 @@ title = '注音高手';
 
 //------------------------------
 //出題時是否要以亂數來選題
+//  true :亂數出題  fasle :按題庫順序
 //------------------------------
-order_by_random = true;		//true:亂數出題 fasle:按題庫順序
-random_order_enabled = 'yes'; //Flash 版的參數名稱
+order_by_random = true;
 
 //------------------------------
 //虛擬鍵盤是否要選國字
+//  true :需自己選國字  fasle :自動選字
 //------------------------------
-character_select_enabled = 'yes';
+character_select_enabled = true;
 
 //------------------------------
 //題目中是否顯示國字
+//  true :顯示國字  fasle :國字變成框框
 //------------------------------
-show_character = 'yes';
+show_character = true;
 
 //------------------------------
-//設定每一回要出多少題給使用者做答
+//每一回出多少題給使用者做答
+//  如果題庫題數不夠，則以題庫數為主
 //------------------------------
-questions_to_answer = 1;
+questions_to_answer = 10;
 
-//------------------------------
-//設定圖片檔和聲音檔的基本路徑
-//------------------------------
-//data_folder_name = 'data';
-
-//------------------------------
-//題庫檔的檔名
-//------------------------------
-//file_list = 'list.js';
 
 //------------------------------
 //接收作答記錄的程式網址
@@ -58,10 +52,10 @@ phonetics_table_filename = 'phonetab.js';
 //------------------------------
 //聲音設定
 //------------------------------
-//答對
+//答對(設多個，可以隨機選)
 sound_ok_mp3 = ['assets/good0.mp3', 'assets/good1.mp3', 'assets/good2.mp3', 'assets/good3.mp3'];
 
-//答錯
+//答錯(設多個，可以隨機選)
 sound_ng_mp3 = ['assets/bad0.mp3', 'assets/bad1.mp3', 'assets/bad2.mp3', 'assets/bad3.mp3'];
 
 sound_click_mp3 = 'assets/sound_click.mp3';

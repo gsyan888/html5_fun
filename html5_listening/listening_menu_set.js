d230f716bb3858cda79d538979bbff8971b588b7
gsyan888 <gsyan888+github@gmail.com>
2022-11-07 15:10:12 +0800

    listening update to 2022 version

--- html5_listening/listening_menu_set.js
+++ html5_listening/listening_menu_set.js
@@ -1,8 +1,6 @@
 ﻿menu_title='聽音辨字六選一';
 menu_items = new Array(
-  '範例(.js),sample_listening_set.js'
-, '範例(.txt),sample_listening_set.txt'
-, '範例(中文測試),sample_chinese_set.js'
-, '範例(2022版),questions-set-1.js'
-, '範例(網路的設定),http://mail.lsps.tp.edu.tw/~gsyan/works/flash/listening/listening2_set.txt'
+  '範例1:綜合展示,questions-set-1.js'
+,  '範例2:只放圖片檔名,questions-set-2.js'
+,  '範例3:只放語音檔名、資料夾chinese,questions-set-3.js'
 );

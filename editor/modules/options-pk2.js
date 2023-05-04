f0fbc40d122316dad651845fe5f91dab29b5a8d2
gsyan888 <gsyan888+github@gmail.com>
2023-05-04 21:51:27 +0800

    add pk2 embed config files

---
+++ editor/modules/options-pk2.js
@@ -0,0 +1,30 @@
+var editorOptions = {
+  pk2: {
+    caption: "PK2:四人PK",
+	
+    filenames: [
+      "https://gsyan888.github.io/html5_fun/html5_pk2/questions-set-1.js"
+    ],
+	
+    enableOpenInNewWindow: false,
+	
+    options: {
+
+      "optionsTotal": [
+        "text",
+        "共有幾個選項"],
+
+      "optionColTotal": [
+        "text",
+        "一列有幾個選項"],
+
+      "helpText": [
+        "text",
+        "開始比賽前, 說明對話框的說明文字"],	  
+	  
+      "questionLines": [
+        "textarea",
+        "題庫"]
+    }
+  }
+};

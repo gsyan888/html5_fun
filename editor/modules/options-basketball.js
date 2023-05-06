55b80d4d7eba869618da16c9610688828da4a944
gsyan888 <gsyan888+github@gmail.com>
2023-05-06 16:18:33 +0800

    Create options-basketball.js

---
+++ editor/modules/options-basketball.js
@@ -0,0 +1,32 @@
+var editorOptions = {
+  basketball: {
+    caption: "籃球高手",
+	
+    filenames: [
+      "https://gsyan888.github.io/html5_fun/editor/templates/pk-q-set-1.js",
+	  "https://gsyan888.github.io/html5_fun/editor/templates/pk-q-set-2.js",
+	  "https://gsyan888.github.io/html5_fun/editor/templates/pk-q-set-3.js"
+    ],
+	
+    enableOpenInNewWindow: false,
+	
+    options: {
+
+      "optionsTotal": [
+        "text",
+        "共有幾個選項"],
+
+      "optionColTotal": [
+        "text",
+        "一列有幾個選項"],
+
+      "helpText": [
+        "text",
+        "開始比賽前, 說明對話框的說明文字"],	  
+	  
+      "questionLines": [
+        "textarea",
+        "題庫"]
+    }
+  }
+};

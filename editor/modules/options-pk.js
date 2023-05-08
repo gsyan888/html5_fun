cbc0bc4bb43e0a132fa0ae7e6169b3ce7d132247
gsyan888 <gsyan888+github@gmail.com>
2023-05-08 12:03:47 +0800

    pk support embed feature

---
+++ editor/modules/options-pk.js
@@ -0,0 +1,33 @@
+var editorOptions = {
+  pk: {
+    caption: "PK大賽",
+	
+    filenames: [
+      "https://gsyan888.github.io/html5_fun/editor/templates/pk-q-set-1.js",
+	  "https://gsyan888.github.io/html5_fun/editor/templates/pk-q-set-2.js",
+	  "https://gsyan888.github.io/html5_fun/editor/templates/pk-q-set-3.js",
+	  "https://gsyan888.github.io/html5_fun/editor/templates/pk-q-set-4.js"
+    ],
+	
+    enableOpenInNewWindow: false,
+	
+    options: {
+
+      "optionsTotal": [
+        "text",
+        "最多有幾個選項(最多6個)"],
+
+      "optionColTotal": [
+        "text",
+        "一列有幾個選項"],
+
+      "helpText": [
+        "text",
+        "開始比賽前, 說明對話框的說明文字"],	  
+	  
+      "questionLines": [
+        "textarea",
+        "題庫"]
+    }
+  }
+};

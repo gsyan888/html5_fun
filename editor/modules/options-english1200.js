452b9638dd51b069db96484075b048123c65b911
gsyan888 <gsyan888+github@gmail.com>
2023-06-08 12:05:19 +0800

    update english1200 template and options

--- editor/modules/options-english1200.js
+++ editor/modules/options-english1200.js
@@ -27,7 +27,8 @@ var editorOptions = {
 
       "numberOfQuestionsPerRound": [
         "text",
-        "每回合抽幾題題目"],
+        "每回合最多抽幾題題目",
+		"(題庫題目總數大於此數時才有作用)"],
 
       "select_questions_in_random": [
         "checkbox",
@@ -40,10 +41,15 @@ var editorOptions = {
         "語音播放的速度",
         "例如:1.0 正常, 0.75 較慢速, 0.5 慢速, 1.5 快速, 2.0 兩倍速"],
 
-      "naudioAutoPlayDelay": [
+      "audioAutoPlayLoop": [
         "text",
-        "隔多久重播(單位秒)"],
+        "語音自動播放的次數(大於1才會重播)"],
 
+      "audioAutoPlayDelay": [
+        "text",
+        "隔多久重播(單位秒)",
+		"(語音自動播放的次數大於 1 時，此參數才有作用)"],
+		
       "tts_enabled": [
         "checkbox",
         "是否使用 TTS 的語音",

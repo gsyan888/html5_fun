22d1f4b22ab3740146521d0f6ec989bdd35c59b6
gsyan888 <gsyan888+github@gmail.com>
2023-07-03 21:26:30 +0800

    frog embed templates update

--- editor/modules/options-frog.js
+++ editor/modules/options-frog.js
@@ -4,16 +4,16 @@ var editorOptions = {
 	
     "templates": [{
         "name": "出題光碟格式(單選題): 題幹##解答##選項1##選項2##選項3##選項4",
-        "path": "https://gsyan888.github.io/html5_fun/editor/templates/football-q-set-1.js"
+        "path": "https://gsyan888.github.io/html5_fun/editor/templates/frog-q-set-1.js"
       }, {
         "name": "兩欄式題庫(單選題), 自動產生錯的選項): 題目##答案",
-        "path": "https://gsyan888.github.io/html5_fun/editor/templates/football-q-set-2.js"
+        "path": "https://gsyan888.github.io/html5_fun/editor/templates/frog-q-set-2.js"
       }, {
         "name": "三欄式題庫(多選題): 題目##正確1~~正確2##錯誤1~~錯誤2~~錯誤3",
-        "path": "https://gsyan888.github.io/html5_fun/editor/templates/football-q-set-3.js"
+        "path": "https://gsyan888.github.io/html5_fun/editor/templates/frog-q-set-3.js"
       }, {
         "name": "語文高手題庫(單選題): 同音字、多音字或形近字",
-        "path": "https://gsyan888.github.io/html5_fun/editor/templates/football-q-set-4.js"
+        "path": "https://gsyan888.github.io/html5_fun/editor/templates/frog-q-set-4.js"
       }
     ],
 	
@@ -28,7 +28,7 @@ var editorOptions = {
       "scoreToMinus": [
         "text",
         "答錯時扣幾分"],
-/*
+
       "theNumberOfQuestions": [
         "text",
         "總共出多少題(可不設定)",
@@ -38,7 +38,7 @@ var editorOptions = {
         "text",
         "青蛙的圖案路徑(可不設定)",
 		"未設定時，使用預設的圖案"],
-*/		
+
       "questionLines": [
         "textarea",
         "題庫"]

4de6b9864164f840179d3a01e28210d745922a07
gsyan888 <gsyan888+github@gmail.com>
2025-04-02 10:42:42 +0800

    maze add tts_lanuage option

--- editor/modules/options-maze.js
+++ editor/modules/options-maze.js
@@ -21,6 +21,12 @@ var editorOptions = {
         "是否使用下一題來當錯的選項",
         "勾選:用下一題 &nbsp;&nbsp; 未勾選:用同一題(難度較高)",
         "以下一題的字當錯的選項"],
+
+      "tts_language": [
+        "text",
+        "念題目時，TTS語音合成的語言代碼",
+        "(例: 英文 <span class=\"btn\" onclick=\"setThisValue(this);\">en-US</span>  、中文 <span class=\"btn\" onclick=\"setThisValue(this);\">zh-TW</span>  、日文 <span class=\"btn\" onclick=\"setThisValue(this);\">ja-JP</span>  、韓文 <span class=\"btn\" onclick=\"setThisValue(this);\">ko-KR</span> <a href=\"https: //cloud.google.com/speech-to-text/docs/languages\" target=\"_blank\">查代碼</a>"
+      ],
 		
       "questionLines": [
         "textarea",
